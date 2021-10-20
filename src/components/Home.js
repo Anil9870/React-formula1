c26a9298d0571af76f853937a656519ffaa10372
Rapuru <anil.rapuru@capgemini.com>
2021-10-20 17:44:10 +0530

    home page modifications

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import HomeCarousel from './HomeCarousel';
+import { Card, Button } from 'react-bootstrap';
 
 
-const formulaImages = ["images/f1.jpg", "images/f2.jpg", "images/f3.jpg", "images/f4.jpg", "images/f5.jpg"]
+const formulaImages = 10;
 
 const Home = props => {
     return (
         <div className="container">
-            <HomeCarousel items={formulaImages} />
+            <div className="home-corousel">
+                <HomeCarousel items={formulaImages} />
+            </div>
             <div className="row">
                 <div className="col sm-4">
-                    <img width="100%" src="images/max1.jpg" alt="max" />
+                    <img className="home-img" src="images/home1.jpg" alt="max" />
                 </div>
                 <div className="col sm-4">
-                    <img width="100%" src="images/max1.jpg" alt="max" />
+                    <img className="home-img" src="images/home2.jpg" alt="max" />
                 </div>
                 <div className="col sm-4">
-                    <img width="100%" src="images/max1.jpg" alt="max" />
+                    <img className="home-img" src="images/home3.jpg" alt="max" />
                 </div>
+            </div>
+            <div style={{ marginTop: '20px', marginBottom: '30px' }} className="row">
+                <Card>
+                    <div className="row">
+                        <div className="col-5">
+                            <Card.Img className="home-img" src="images/mick.jpg" />
+                        </div>
+                        <div className="col-5">
+                            <Card.Body>
+                                <Card.Title><h1>Meet the king of F1</h1></Card.Title>
+                                <Card.Text style={{ marginTop: '20px' }}>
+                                    <p style={{ fontSize: '20px', textAlign: 'center' }}>Legend in F1.Won total 7 championships
+                                        Schumacher was noted for pushing his car to the very limit for sustained periods during races, a pioneering fitness regimen and ability to galvanise teams around him. He and his younger brother Ralf are the only siblings to win races in Formula One and the first siblings to finish first and second in the same race, a feat they repeated in four subsequent races.
+                                    </p>
+                                </Card.Text>
+                                <Button style={{ marginTop: '30px' }} variant="info" size="lg"><a href="https://en.wikipedia.org/wiki/Michael_Schumacher" target="blank">View More Info</a></Button>
+                            </Card.Body>
+                        </div>
+                    </div>
+                </Card>
             </div>
         </div >
     )
--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,15 +1,31 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import Carousel from 'react-elastic-carousel';
 
-const HomeCarousel = ({items}) => {
+const HomeCarousel = ({ items }) => {
+    const renderImages = () => {
+        const images = [];
+        for (var i = 1; i <= 10; i++) {
+            let element = <img src={`images/corousel${i}.png`} alt={`corousel${i}`} />
+            images.push(element);
+        }
+        return images;
+    }
     return (
         <div className="container">
-            <h2>TEAMS</h2>
-            <Carousel>
-                {items.map((item) => {
-                    return <img width="300px" height="300px" src={item} alt={item} />
-                })}
-            </Carousel>
+            <div className="row">
+                <div className="col-6">
+                    <h2>TEAMS</h2>
+                </div>
+                <div className="col-6">
+                    <h2>WE RACE FOR UNITY</h2>
+                </div>
+                <div>
+                    <Carousel>
+                        {renderImages()}
+                    </Carousel>
+                </div>
+            </div>
         </div>
     )
 }
